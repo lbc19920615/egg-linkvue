3a9fc280f6e8a26430ba49fcfe117b376ec87029
凌柏超 <1585638808@qq.com>
2021-08-15 20:21:06 +0800

    添加ext.js

---
+++ fronts/ext.js
@@ -0,0 +1,7 @@
+import _eval5 from 'eval5';
+
+/**
+ * eval5
+ * @type {(code: string, ctx?: (VMContext | undefined), options?: (ScriptOptions | undefined)) => any}
+ */
+export const eval5 = _eval5;
