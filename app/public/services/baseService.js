44a34c860558a720f0e3131e564c25ab6f2bafb7
凌柏超 <1585638808@qq.com>
2021-08-21 14:35:41 +0800

    Update baseService.js

--- app/public/services/baseService.js
+++ app/public/services/baseService.js
@@ -27,37 +27,41 @@ export const baseServiceDef = ({ vue, props, config }) => {
   }
 
   function initModel(modelDef = { plain: {}, computed: {}, callback: {}, context: {} }) {
-    scope.run(() => {
-      model = reactive(modelDef.plain);
+    // scope.run(() => {
+    //
+    // });
 
-      for (const computedKey in modelDef.computed) {
-        const fun = modelDef.computed[computedKey](model, {
-          beforeBuild(key) {
+    model = reactive(modelDef.plain);
+
+    for (const computedKey in modelDef.computed) {
+      const fun = modelDef.computed[computedKey](model, {
+        beforeBuild(key) {
+          // console.log('beforeBuild', key, computedModel[key].value);
+          const preValue = computedModel[key].value;
+          nextTick(() => {
             // console.log('beforeBuild', key, computedModel[key].value);
-            const preValue = computedModel[key].value;
-            nextTick(() => {
-              // console.log('beforeBuild', key, computedModel[key].value);
-              const nextValue = computedModel[key].value;
-              if (modelDef.callback && modelDef.callback.onComputedChange) {
-                modelDef.callback.onComputedChange(key, nextValue, preValue);
-              }
-            });
-          },
-          context: modelDef.context,
-        });
-        computedModel[computedKey] = computed(fun);
-      }
+            const nextValue = computedModel[key].value;
+            if (modelDef.callback && modelDef.callback.onComputedChange) {
+              modelDef.callback.onComputedChange(key, nextValue, preValue);
+            }
+          });
+        },
+        context: modelDef.context,
+      });
+      computedModel[computedKey] = computed(fun);
+    }
 
 
-      // watch(() => model.name, () => console.log(model, computedModel));
-      global.ZY.PubSub.publish(globalStore.EVENT_TYPES.INIT_MODEL_READY, '');
-      ctx.RefsManager.emit(globalStore.EVENT_TYPES.INIT_MODEL_READY, { ctx });
-    });
+    // watch(() => model.name, () => console.log(model, computedModel));
+    global.ZY.PubSub.publish(globalStore.EVENT_TYPES.INIT_MODEL_READY, '');
+    ctx.RefsManager.emit(globalStore.EVENT_TYPES.INIT_MODEL_READY, { ctx });
+
     return getModel();
   }
 
   async function setModel(newVal) {
     // console.log(ZY.lodash.get(model, path));
+    console.log('setModel', newVal)
     for (const key in newVal) {
       model[key] = newVal[key];
     }
