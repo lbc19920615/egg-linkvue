c00ed2e327e4c1b178c7218e611ec85c5e74314d
baichao ling <1585638808@qq.com>
2021-08-23 16:42:26 +0800

    支持了 form arr的一些列操作

--- app/core/controller.js
+++ app/core/controller.js
@@ -1,8 +1,16 @@
+function getSelfPath(basePath, BASE_PATH) {
+  let fromPath = basePath.replace(BASE_PATH, '');
+  if (fromPath.startsWith('.')) {
+    fromPath = fromPath.slice(1, fromPath.length);
+  }
+  return fromPath;
+}
+
 function renderForm(p, basePath, configPath, append = {}) {
   const context = {
     tpl: '',
   };
-  function render(p, key, context, level, basePath, configPath) {
+  function render(p, key, context, level, basePath, configPath, ext) {
     if (p.type === 'object') {
       context.tpl = context.tpl + `
 <el-row class="level_${level} object"
@@ -10,28 +18,35 @@ v-if="${basePath}"
 >`;
       for (const [ key, value ] of Object.entries(p.properties)) {
         render(value, key, context, level + 1,
-          `${basePath}['${key}']`, `${configPath}.properties['${key}']`);
+          `${basePath}.${key}`, `${configPath}.properties.${key}`, ext);
       }
       context.tpl = context.tpl + `
 </el-row>`;
     } else if (p.type === 'array') {
       const itemKey = 'item' + level;
       const indexKey = 'index' + level;
+      const fromPath = getSelfPath(basePath, append.BASE_PATH);
       context.tpl = context.tpl + `
 <el-row class="level_${level} array">
  <slot-com :defs="slotContent" :attrs="{parts}"
-           :binds="{key: '${key}', partName: '${append.part.name}', process: '${append.CONFIG.process}', parts: parts, BASE_PATH:'${append.BASE_PATH}' }"
+           :binds="{key: '${key}', partName: '${append.part.name}', configPath: '${configPath}', selfpath: '${fromPath}', process: '${append.CONFIG.process}', parts: parts, BASE_PATH:'${append.BASE_PATH}' }"
               name="array_before"></slot-com>
 <template v-for="(${itemKey}, ${indexKey}) in ${basePath}" >`;
       if (p.items.type === 'object') {
         for (const [ key, value ] of Object.entries(p.items.properties)) {
           render(value, key, context, level + 1,
-            `${basePath}[${indexKey}]['${key}']`, `${configPath}.items.properties['${key}']`);
+            `${basePath}[${indexKey}].${key}`, `${configPath}.items.properties.${key}`, ext);
         }
         // render(p.items, context, level + 1)
       }
       context.tpl = context.tpl + `
+<slot-com :defs="slotContent" :attrs="{parts}"
+         :binds="{key: '${key}', partName: '${append.part.name}', indexKey:${indexKey}, fromPath: '${fromPath}', selfpath: '${fromPath}['+ ${indexKey} +']', level:'${level}', parentlevel:'${level - 1}', basePath: '${basePath}', configPath: '${configPath}', process: '${append.CONFIG.process}', parts: parts, BASE_PATH:'${append.BASE_PATH}' }"
+            name="array_item_after"></slot-com>
 </template>
+<slot-com :defs="slotContent" :attrs="{parts}"
+         :binds="{key: '${key}', partName: '${append.part.name}', configPath: '${configPath}', process: '${append.CONFIG.process}', parts: parts, BASE_PATH:'${append.BASE_PATH}' }"
+            name="array_after"></slot-com>
 </el-row>`;
     } else {
       // console.log(p, key);
@@ -50,11 +65,14 @@ type="${p.type}"
 >
 </cm-field>`;
       context.tpl = context.tpl + `
+ <slot-com :defs="slotContent" :attrs="{parts}"
+           :binds="{key: '${key}', type: '${p.type}', partName: '${append.part.name}', level:'${level}', parentlevel:'${level - 1}', basePath: '${basePath}', configPath: '${configPath}', process: '${append.CONFIG.process}', parts: parts, BASE_PATH:'${append.BASE_PATH}' }"
+              name="prop_after"></slot-com>
 </el-col>`;
     }
   }
 
-  render(p, '', context, 1, basePath, configPath);
+  render(p, '', context, 1, basePath, configPath, { arrIndexes: {} });
   return context.tpl;
 }
 
@@ -65,7 +83,7 @@ class BaseController extends Controller {
     super(ctx);
   }
   BASE_renderForm(config, basePath, configPath, append) {
-    append.BASE_PATH = basePath
+    append.BASE_PATH = basePath;
     return renderForm(config, basePath, configPath, append);
   }
 }
