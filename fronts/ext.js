61741769f10a64ccdbd18beec46dd31735a1a497
baichao ling <1585638808@qq.com>
2021-09-03 16:44:05 +0800

    ext添加一些方法
    1. saveAs
    2. saveObjAsJson5File

--- fronts/ext.js
+++ fronts/ext.js
@@ -1,4 +1,5 @@
-import _eval5, { Interpreter } from 'eval5';
+import _eval5 from 'eval5';
+import _JSON5 from 'json5';
 
 /**
  * eval5
@@ -29,3 +30,38 @@ import _local from 'localforage';
  * @type {LocalForage}
  */
 export const store = _local;
+
+
+import _fileUtils from 'file-saver';
+
+/**
+ * saveAs
+ */
+export const saveAs = _fileUtils.saveAs;
+
+/**
+ * saveStrAs
+ * @param str
+ * @param file
+ * @param type
+ * @param cls
+ * @return {*}
+ */
+export function saveStrAs(str = '', {
+  file = '',
+  type = 'text/plain;charset=utf-8',
+  cls = Blob,
+} = {}) {
+  const blob = new cls([ str ], { type });
+  return saveAs(blob, file);
+}
+
+/**
+ * 存贮js对象为json5文件
+ * @param obj
+ */
+export function saveObjAsJson5File(obj = {}, fileName) {
+  saveStrAs(_JSON5.stringify(obj), {
+    file: fileName + '.json5',
+  });
+}
