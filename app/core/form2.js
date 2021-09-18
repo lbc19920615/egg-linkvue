54963f232f75ccb9ce8cfb33aeb572232f977d01
baichao ling <1585638808@qq.com>
2021-09-18 11:41:55 +0800

    Update form2.js
    uniapp 时间处理下

--- app/core/form2.js
+++ app/core/form2.js
@@ -13,24 +13,30 @@ label="${key}"
 </${field_tag}>`;
 
   if (p.ui && p.ui.widgetFormat) {
-      console.log(p.ui.widgetFormat, p.ui)
-  }
-
-  if (field_tag === 'van-field') {
-    let afterStr = '';
-    if (p.type === 'number' || p.type === 'integer') {
-      const attrs = [
-        [ 'type', 'number' ],
-      ];
-      afterStr = attrTOStr(attrs);
-      console.log(afterStr);
+    // console.log(p.ui.widgetFormat)
+    if (p.ui.widgetFormat === 'datetime') {
+      ret = `<zv-van-field label="${key}"><zv-date-time-picker
+:datetime="${basePath}"
+@change="onSetProp([${append.BASE_FORM_KEY} ${pathArrStr}], $event)">
+</zv-date-time-picker></zv-van-field>`;
     }
-    ret = `<van-field
+  } else {
+    if (field_tag === 'van-field') {
+      let afterStr = '';
+      if (p.type === 'number' || p.type === 'integer') {
+        const attrs = [
+          [ 'type', 'number' ],
+        ];
+        afterStr = attrTOStr(attrs);
+        console.log(afterStr);
+      }
+      ret = `<van-field
 v-model="${basePath}"
 label="${key}"
  ${afterStr}
 @change="onSetProp([${append.BASE_FORM_KEY} ${pathArrStr}], $event)">
 </van-field>`;
+    }
   }
 
 
