a1bf8b36cdee834e13df3f44349eff7948b39cb4
baichao ling <1585638808@qq.com>
2021-11-19 10:46:39 +0800

    Update controller.js

--- app/core/controller.js
+++ app/core/controller.js
@@ -182,7 +182,7 @@ v-if="${basePath}"
       const con_cls = buildCls(p, 'ui.conClass');
       // console.dir(append.CONFIG)
       if (wrap_tag) {
-        context.tpl = context.tpl + `<${wrap_tag} :binds="{ pathArr: [${pathArrStr.slice(1)}] }" ${attrStr(p, 'wrapAttrs')}>`;
+        context.tpl = context.tpl + `<${wrap_tag} :binds="{ key: '${key}',  label: '${getLabel(append.CONFIG, configPath, key)}',   config: getUI_CONFIG('${configPath}'),  selfpath: '${fromPath}',  partName: '${append.part.name}', pathArr: [${pathArrStr.slice(1)}] }" ${attrStr(p, 'wrapAttrs')}>`;
       }
 
       context.tpl = context.tpl + `
