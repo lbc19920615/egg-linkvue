a7420137025a2bfc83ad79e122372f370e29c24f
baichao ling <1585638808@qq.com>
2021-12-16 11:03:28 +0800

    添加了resize observer

--- fronts/vue2.js
+++ fronts/vue2.js
@@ -9,3 +9,9 @@ import _eval5 from 'eval5';
  * eval5官网 {@link https://www.npmjs.com/package/eval5}
  */
 export const eval5 = _eval5;
+
+/**
+ * {ResizeObserver, ResizeObserverEntry, ResizeObserverSize}
+ */
+import * as Lib from '@juggle/resize-observer';
+export const ResizeObserver = Lib.ResizeObserver;
