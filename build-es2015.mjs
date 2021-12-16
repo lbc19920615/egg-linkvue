b9ea7bd8287045aa72c22c6eeda0423cfead0399
baichao ling <1585638808@qq.com>
2021-12-16 14:05:57 +0800

    添加老浏览器支持的版本

---
+++ build-es2015.mjs
@@ -0,0 +1,50 @@
+import GlobalsPlugin from 'esbuild-plugin-globals';
+import esbuild from 'esbuild';
+
+(async () => {
+  // const vue3Plugin = s.vue3Plugin;
+
+  /** Mapping from module paths to global variables */
+  const globals = {
+    vue: {
+      type: 'esm',
+      defaultExport: false,
+    },
+  };
+
+
+  try {
+    const res = await esbuild.build({
+      // entryPoints: [ './fronts/main.js', './fronts/sds.js' ],
+      entryPoints: [ './fronts/main.js', './fronts/vue2.js'  ],
+      // outfile: './app/public/expose.js',
+      // minify: true,
+      // splitting: true,
+      outdir: './app/public/expose/old',
+      format: 'cjs',
+      bundle: true,
+      target: [
+        'es2016',
+      ],
+      chunkNames: 'chunks/[name]-[hash]',
+      external: [
+        'vue',
+      ],
+      plugins: [
+        // vue3Plugin(),
+        GlobalsPlugin({
+          /**
+           * Simple string pattern
+           * Any module matching "react" will be replaced with
+           * `module.exports = React`
+           */
+          vue: "window.Vue",
+        }),
+      ],
+      // write: false,
+    });
+    console.log(res);
+  } finally {
+  //
+  }
+})();
